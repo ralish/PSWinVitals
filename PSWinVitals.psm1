e7045feed8faf11ba9d41c95e7772c6bd4cfb524
Samuel D. Leslie <sdl@nexiom.net>
2019-01-25 12:40:30 +1100

    Sync Get-InstalledPrograms with improvements in PSWinGlue

--- PSWinVitals.psm1
+++ PSWinVitals.psm1
@@ -730,81 +730,93 @@ Function Get-InstalledPrograms {
     [CmdletBinding()]
     Param()
 
-    $NativeRegPath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
-    $Wow6432RegPath = 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+    $Results = @()
+    $TypeName = 'PSWinVitals.InstalledProgram'
 
-    $UninstallKeys = Get-ChildItem -Path $NativeRegPath
-    if (Test-Path -Path $Wow6432RegPath -PathType Container) {
-        $UninstallKeys += Get-ChildItem -Path $Wow6432RegPath
+    Update-TypeData -TypeName $TypeName -DefaultDisplayPropertySet @('Name', 'Publisher', 'Version') -Force
+
+    # Programs installed system-wide in native bitness
+    $ComputerNativeRegPath = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall'
+    # Programs installed system-wide under the 32-bit emulation layer (64-bit Windows only)
+    $ComputerWow64RegPath = 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+
+    # Retrieve all installed programs from available keys
+    $UninstallKeys = Get-ChildItem -Path $ComputerNativeRegPath
+    if (Test-Path -Path $ComputerWow64RegPath -PathType Container) {
+        $UninstallKeys += Get-ChildItem -Path $ComputerWow64RegPath
     }
 
-    $InstalledPrograms = @()
+    # Filter out all the uninteresting installation results
     foreach ($UninstallKey in $UninstallKeys) {
         $Program = Get-ItemProperty -Path $UninstallKey.PSPath
 
         # Skip any program which doesn't define a display name
-        if ($Program.PSObject.Properties['DisplayName']) {
-            # Ensure the program either:
-            # - Has an uninstall command
-            # - Is marked as non-removable
-            if (!($Program.PSObject.Properties['UninstallString'] -or ($Program.PSObject.Properties['NoRemove'] -and $Program.NoRemove -eq 1))) {
-                continue
-            }
-
-            # Skip any program which defines a parent program
-            if ($Program.PSObject.Properties['ParentKeyName'] -or $Program.PSObject.Properties['ParentDisplayName']) {
-                continue
-            }
-
-            # Skip any program marked as a system component
-            if ($Program.PSObject.Properties['SystemComponent'] -and $Program.SystemComponent -eq 1) {
-                continue
-            }
-
-            # Skip any program which defines a release type
-            if ($Program.PSObject.Properties['ReleaseType']) {
-                continue
-            }
-
-            $InstalledProgram = [PSCustomObject]@{
-                Name = $Program.DisplayName
-                Publisher = $null
-                InstallDate = $null
-                EstimatedSize = $null
-                Version = $null
-                Location = $null
-                Uninstall = $null
-            }
-
-            if ($Program.PSObject.Properties['Publisher']) {
-                $InstalledProgram.Publisher = $Program.Publisher
-            }
-
-            if ($Program.PSObject.Properties['InstallDate']) {
-                $InstalledProgram.InstallDate = $Program.InstallDate
-            }
-
-            if ($Program.PSObject.Properties['EstimatedSize']) {
-                $InstalledProgram.EstimatedSize = $Program.EstimatedSize
-            }
-
-            if ($Program.PSObject.Properties['DisplayVersion']) {
-                $InstalledProgram.Version = $Program.DisplayVersion
-            }
-
-            if ($Program.PSObject.Properties['InstallLocation']) {
-                $InstalledProgram.Location = $Program.InstallLocation
-            }
-
-            if ($Program.PSObject.Properties['UninstallString']) {
-                $InstalledProgram.Uninstall = $Program.UninstallString
-            }
-
-            $InstalledPrograms += $InstalledProgram
+        if (!$Program.PSObject.Properties['DisplayName']) {
+            continue
         }
+
+        # Ensure the program either:
+        # - Has an uninstall command
+        # - Is marked as non-removable
+        if (!($Program.PSObject.Properties['UninstallString'] -or ($Program.PSObject.Properties['NoRemove'] -and $Program.NoRemove -eq 1))) {
+            continue
+        }
+
+        # Skip any program which defines a parent program
+        if ($Program.PSObject.Properties['ParentKeyName'] -or $Program.PSObject.Properties['ParentDisplayName']) {
+            continue
+        }
+
+        # Skip any program marked as a system component
+        if ($Program.PSObject.Properties['SystemComponent'] -and $Program.SystemComponent -eq 1) {
+            continue
+        }
+
+        # Skip any program which defines a release type
+        if ($Program.PSObject.Properties['ReleaseType']) {
+            continue
+        }
+
+        $Result = [PSCustomObject]@{
+            PSTypeName      = $TypeName
+            PSPath          = $Program.PSPath
+            Name            = $Program.DisplayName
+            Publisher       = $null
+            InstallDate     = $null
+            EstimatedSize   = $null
+            Version         = $null
+            Location        = $null
+            Uninstall       = $null
+        }
+
+        if ($Program.PSObject.Properties['Publisher']) {
+            $Result.Publisher = $Program.Publisher
+        }
+
+        if ($Program.PSObject.Properties['InstallDate']) {
+            $Result.InstallDate = $Program.InstallDate
+        }
+
+        if ($Program.PSObject.Properties['EstimatedSize']) {
+            $Result.EstimatedSize = $Program.EstimatedSize
+        }
+
+        if ($Program.PSObject.Properties['DisplayVersion']) {
+            $Result.Version = $Program.DisplayVersion
+        }
+
+        if ($Program.PSObject.Properties['InstallLocation']) {
+            $Result.Location = $Program.InstallLocation
+        }
+
+        if ($Program.PSObject.Properties['UninstallString']) {
+            $Result.Uninstall = $Program.UninstallString
+        }
+
+        $Results += $Result
     }
 
-    return $InstalledPrograms
+    return ($Results | Sort-Object -Property Name)
 }
 
 Function Get-KernelCrashDumps {
